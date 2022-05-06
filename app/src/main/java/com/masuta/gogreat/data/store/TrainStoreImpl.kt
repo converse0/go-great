5218d4fe159c0152de8159256b636918ca8dae45
converse01 <kvasyuk.serezha@mail.ru>
2022-05-06 20:19:26 +0300

    train store impl

--- app/src/main/java/com/masuta/gogreat/data/store/TrainStoreImpl.kt
+++ app/src/main/java/com/masuta/gogreat/data/store/TrainStoreImpl.kt
@@ -1,14 +1,17 @@
 package com.masuta.gogreat.data.store
 
 import android.content.Context
-import com.masuta.gogreat.domain.model.LoginResponse
-import com.masuta.gogreat.domain.model.refreshUserToken
-import com.masuta.gogreat.domain.model.userToken
+import androidx.compose.runtime.mutableStateOf
+import com.masuta.gogreat.domain.model.Training
 import javax.inject.Inject
 
-class StoreImpl @Inject constructor(
+class TrainStoreImpl @Inject constructor(
     private val context: Context
-) : Store {
+) : TrainStore {
+
+    private var localWorkouts: List<Training> = mutableListOf()
+    private var localCurrentWorkout = mutableStateOf<Training?>(null)
+    private var localPastWorkouts: List<Training> = mutableListOf()
 
     private val sharedPref = context.getSharedPreferences("user", Context.MODE_PRIVATE)
 
@@ -39,4 +42,29 @@ class StoreImpl @Inject constructor(
         editor.putInt("currentExerciseSets", exerciseSets ?: -1)
         editor.apply()
     }
+
+    override suspend fun getLocalWorkouts(): List<Training> {
+        return localWorkouts
+    }
+
+    override suspend fun setLocalWorkouts(workouts: List<Training>) {
+        localWorkouts = workouts
+    }
+
+    override suspend fun getLocalCurrentWorkout(): Training? {
+        return localCurrentWorkout.value
+    }
+
+    override suspend fun setLocalCurrentWorkout(workout: Training?) {
+        localCurrentWorkout.value = workout
+    }
+
+    override suspend fun getLocalPastWorkouts(): List<Training> {
+        return localPastWorkouts
+    }
+
+    override suspend fun setLocalPastWorkouts(workouts: List<Training>) {
+        localPastWorkouts = workouts
+    }
+
 }
